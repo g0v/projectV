3d448f1549acf2ea49eb68568d4eec31ad83493d
Mark Chang <mark23456@hotmail.com>
2014-10-20 21:00:42 +0800

    mission hp and sp

--- app/scripts/controllers/mission.js
+++ app/scripts/controllers/mission.js
@@ -12,9 +12,40 @@ angular.module('projectVApp')
 
   ['$scope', '$route', '$routeParams','voteInfoService',
   function ($scope, $route, $routeParams, voteInfoService ) {
+
+    var county = $routeParams.county;
+
+    $scope.miscope = {};
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    
+    $scope.miscope.vCount = 0;
+    $scope.miscope.vTotal = 0;
+    $scope.miscope.sCount = 0;
+    $scope.miscope.sTotal = 0;
+    
+    voteInfoService.getAllVoteStatInfo(county).then(function(data){
+      console.log('--data--',data);
+      var vCount = 0;
+      var vTotal = 0;
+      var sCount = 0;
+      var sTotal = 0;
+      for(var key in data){
+        var vtempTotal = voteInfoService.volCount * data[key].vweight;
+        vTotal += vtempTotal;
+        vCount += data[key].vlist.length >= vtempTotal ?  vtempTotal : data[key].vlist.length;
+        for(var item in voteInfoService.supplementItem ){
+          var stempTotal = voteInfoService.supplementItem[item][0] * data[key].vweight;
+          sTotal += stempTotal;
+          sCount += data[key].sItemCount[item] >= stempTotal ? stempTotal : data[key].sItemCount[item];
+        }
+      }
+      $scope.miscope.vCount = vCount;
+      $scope.miscope.vTotal = vTotal;
+      $scope.miscope.sCount = sCount;
+      $scope.miscope.sTotal = sTotal;
+    });
   }]);
